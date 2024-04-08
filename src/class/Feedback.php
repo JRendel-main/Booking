990e6f3023ae7e9e21368f748ff2b733722e1e67
JRendel <mrskybrine@gmail.com>
2024-04-08 23:05:41 +0800

    Refactor Feedback class and viewfeedbacks.php***

--- src/class/Feedback.php
+++ src/class/Feedback.php
@@ -9,14 +9,54 @@ class Feedback
 
     public function getFeedbacks()
     {
-        $sql = "SELECT * FROM feedbacks";
+        $sql = "SELECT a.*, b.* FROM feedbacks a, guests b WHERE a.GuestID = b.GuestID";
         $result = $this->conn->query($sql);
-        $feedbacks = [];
+
         if ($result->num_rows > 0) {
+            $feedbacks = array();
             while ($row = $result->fetch_assoc()) {
                 $feedbacks[] = $row;
             }
+            return $feedbacks;
+        } else {
+            return false;
         }
-        return $feedbacks;
+    }
+
+    public function checkAlreadySubmitted($GuestID)
+    {
+        $sql = "SELECT * FROM feedbacks WHERE GuestID = '$GuestID'";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function checkForFeedback($GuestID)
+    {
+        // query for check on reservation table if the user already done on reservation on column CheckInDate and check status if approved also
+        $sql = "SELECT * FROM reservations WHERE GuestID = '$GuestID' AND status = 'approved'";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function submitFeedback($feedback, $GuestID)
+    {
+        $timestamp = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO feedbacks (feedback, GuestID, timestamp) VALUES ('$feedback', $GuestID, '$timestamp')";
+
+        if ($this->conn->query($sql) === TRUE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
